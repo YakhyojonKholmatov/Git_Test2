d4b28c51d2473e7fdd1d2b2f5c9152f0eefc816b
YakhyojonKholmatov <yakhprogramming@gmail.com>
2023-03-08 19:33:12 +0500

    Birinchi vazifani bajarganimda topshirish uchun xabar beryapman.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    private static void task1(){
+        System.out.println("Birinchi vazifani bajarish");
     }
 }
